9358faa5d6a61bf0867f7d87c3fc18072de6ef67
Sam Soheylee <ssoheyleearasi@gmail.com>
2019-01-12 21:29:58 +0000

    forEach refactor

---
+++ foreachwaldo.js
@@ -0,0 +1,18 @@
+// The second argument/parameter is expected to be a function
+function findWaldo(arr, found) {
+
+
+  arr.forEach(function(element){
+    if(element === "Waldo"){
+      found(arr.indexOf(element));
+    }
+  });
+
+}
+
+function actionWhenFound(index) {
+
+  console.log("Found Waldo at index " + index + "!");
+}
+
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
